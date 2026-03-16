6dfe517cc63c5318ba32e31b84b8f2673eec3ada
Mercyalbina20 <156925658+Mercyalbina20@users.noreply.github.com>
2026-03-16 16:02:37 +0530

    Fix Pydantic Type Error for DEFAULT_DB_URL

--- backend/core/config.py
+++ backend/core/config.py
@@ -7,8 +7,6 @@ class Settings(BaseSettings):
     SECRET_KEY: str = "INSECURE_SECRET_FOR_MOCK_ENV"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     
-    # Check environment variable first, then fallback to local sqlite if nothing
-    DEFAULT_DB_URL = "postgresql+psycopg2://neondb_owner:npg_YjGsvpVc56qy@ep-morning-union-am7gbqih-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
-    SQLALCHEMY_DATABASE_URI: str = os.getenv("DATABASE_URL", DEFAULT_DB_URL)
+    SQLALCHEMY_DATABASE_URI: str = os.getenv("DATABASE_URL", "postgresql+psycopg2://neondb_owner:npg_YjGsvpVc56qy@ep-morning-union-am7gbqih-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
 
 settings = Settings()
